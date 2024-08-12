c9a43c78af7ddd802dbaa5662af55ba4fdf6a15e
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-08-12 19:39:11 +0530

    java

--- basic/a.java
+++
@@ -1,9 +0,0 @@
-package basic;
-
-public class a{
-    public static void main(String[] args) {
-        //Write a Java program to print 'Hello' on screen and your name on a separate line.
-        System.out.println("Hello");
-        System.out.println("Dharshini");
-    }
-}
